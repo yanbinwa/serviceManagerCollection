292aa2e004112985ef0e2ba62f4548299fae4fa6
yanbinwa <yanbinwa@cisco.com>
2017-05-26 09:59:22 +0800

    add kafka function2

--- src/main/java/yanbinwa/iCollection/service/CollectionServiceImpl.java
+++ src/main/java/yanbinwa/iCollection/service/CollectionServiceImpl.java
@@ -1,6 +1,8 @@
 package yanbinwa.iCollection.service;
 
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -8,6 +10,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Service;
 
+import yanbinwa.common.kafka.message.KafkaMessage;
+import yanbinwa.common.kafka.producer.IKafkaProducer;
+import yanbinwa.common.kafka.producer.IKafkaProducerImpl;
 import yanbinwa.common.orchestrationClient.OrchestartionCallBack;
 import yanbinwa.common.orchestrationClient.OrchestrationClient;
 import yanbinwa.common.orchestrationClient.OrchestrationClientImpl;
@@ -24,32 +29,45 @@ public class CollectionServiceImpl implements CollectionService
 
     private static final Logger logger = Logger.getLogger(CollectionServiceImpl.class);
     
-    Map<String, String> serviceDataProperites;
-    Map<String, String> zNodeInfoProperites;
+    Map<String, String> serviceDataProperties;
+    Map<String, String> zNodeInfoProperties;
+    Map<String, String> kafkaProducerProperties;
     
-    public void setServiceDataProperites(Map<String, String> properties)
+    public void setServiceDataProperties(Map<String, String> properties)
     {
-        this.serviceDataProperites = properties;
+        this.serviceDataProperties = properties;
     }
     
-    public Map<String, String> getServiceDataProperites()
+    public Map<String, String> getServiceDataProperties()
     {
-        return this.serviceDataProperites;
+        return this.serviceDataProperties;
     }
     
-    public void setZNodeInfoProperites(Map<String, String> properties)
+    public void setZNodeInfoProperties(Map<String, String> properties)
     {
-        this.zNodeInfoProperites = properties;
+        this.zNodeInfoProperties = properties;
     }
     
-    public Map<String, String> getZNodeInfoProperites()
+    public Map<String, String> getZNodeInfoProperties()
     {
-        return this.zNodeInfoProperites;
+        return this.zNodeInfoProperties;
+    }
+    
+    public void setKafkaProducerProperties(Map<String, String> properties)
+    {
+        this.kafkaProducerProperties = properties;
+    }
+    
+    public Map<String, String> getKafkaProducerProperties()
+    {
+        return this.kafkaProducerProperties;
     }
     
     ZNodeServiceData serviceData = null;
         
     OrchestrationClient client = null;
+    
+    IKafkaProducer kafkaProducer = null;
     
     boolean isRunning = false;
     
@@ -58,21 +76,22 @@ public class CollectionServiceImpl implements CollectionService
     @Override
     public void afterPropertiesSet() throws Exception
     {
-        String zookeeperHostIp = zNodeInfoProperites.get(OrchestrationClient.ZOOKEEPER_HOSTPORT_KEY);
+        String zookeeperHostIp = zNodeInfoProperties.get(OrchestrationClient.ZOOKEEPER_HOSTPORT_KEY);
         if(zookeeperHostIp == null)
         {
             logger.error("Zookeeper host and port should not be null");
             return;
         }
         
-        String serviceName = serviceDataProperites.get(CollectionService.SERVICE_SERVICENAME);
-        String ip = serviceDataProperites.get(CollectionService.SERVICE_IP);
-        String portStr = serviceDataProperites.get(CollectionService.SERVICE_PORT);
+        String serviceName = serviceDataProperties.get(CollectionService.SERVICE_SERVICENAME);
+        String ip = serviceDataProperties.get(CollectionService.SERVICE_IP);
+        String portStr = serviceDataProperties.get(CollectionService.SERVICE_PORT);
         int port = Integer.parseInt(portStr);
-        String rootUrl = serviceDataProperites.get(CollectionService.SERVICE_ROOTURL);
+        String rootUrl = serviceDataProperties.get(CollectionService.SERVICE_ROOTURL);
         serviceData = new ZNodeServiceData(ip, serviceName, port, rootUrl);
         
-        client = new OrchestrationClientImpl(serviceData, watcher, zookeeperHostIp, zNodeInfoProperites);
+        client = new OrchestrationClientImpl(serviceData, watcher, zookeeperHostIp, zNodeInfoProperties);
+        kafkaProducer = new IKafkaProducerImpl(kafkaProducerProperties, new HashSet<String>(), "kafkaProducer");
         start();
     }
 
@@ -156,13 +175,81 @@ public class CollectionServiceImpl implements CollectionService
     
     class OrchestrationWatcher implements OrchestartionCallBack
     {
-
+        
+        OrchestrationServiceState curState = OrchestrationServiceState.NOTREADY;
+        Thread sendMessageThread = null;
+        boolean isSendMessageThreadRun = false;
+        
+        private void sendMessageOneByOne()
+        {
+            while(isSendMessageThreadRun)
+            {
+                KafkaMessage msg = new KafkaMessage(1, "HelloWord");
+                logger.info("Send a msg: " + msg);
+                kafkaProducer.sendMessage(msg);
+                try
+                {
+                    Thread.sleep(5000);
+                } 
+                catch (InterruptedException e)
+                {
+                    if(!isSendMessageThreadRun)
+                    {
+                        logger.info("Stop send message thread");
+                    }
+                    else
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        
+        /**
+         * 如果Ready，就根据dependence开始计算当前的topic list，从而对producer进行update，之后就开启producer，同时定期写入数据
+         * 如果DependChange，就根据dependence更新当前的topic list
+         * 如果UnReady，就停止producer，但是不会丢掉未发出的Msg，一旦Ready，会定期继续发数据
+         */
         @Override
         public void handleServiceStateChange(OrchestrationServiceState state)
         {
             logger.info("Service state is: " + state);
+            //由Unready -> ready
+            if (state == OrchestrationServiceState.READY && curState == OrchestrationServiceState.NOTREADY)
+            {
+                logger.info("The service is started");
+                Set<String> topic = new HashSet<String>();
+                topic.add("wyb");
+                kafkaProducer.updateTopic(topic);
+                kafkaProducer.start();
+                sendMessageThread = new Thread(new Runnable(){
+
+                    @Override
+                    public void run()
+                    {
+                        sendMessageOneByOne();
+                    }
+                    
+                });
+                isSendMessageThreadRun = true;
+                sendMessageThread.start();
+                curState = state;
+            }
+            else if(state == OrchestrationServiceState.NOTREADY && curState == OrchestrationServiceState.READY)
+            {
+                logger.info("The service is stopped");
+                isSendMessageThreadRun = false;
+                sendMessageThread.interrupt();
+                kafkaProducer.stop();
+                curState = state;
+            }
+            else if(state == OrchestrationServiceState.DEPCHANGE)
+            {
+                Set<String> topic = new HashSet<String>();
+                topic.add("wyb");
+                kafkaProducer.updateTopic(topic);
+                logger.info("The dependence is changed");
+            }
         }
-        
     }
-    
 }
